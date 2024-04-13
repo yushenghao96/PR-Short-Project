814c8f37e1455db6360e9c00636e3287d58b3059
SergioFL2 <sergio.fernandez.leon@estudiantat.upc.edu>
2024-04-13 13:37:47 +0200

    ongoing changes

---
+++ FeatureExtraction.py
@@ -0,0 +1,51 @@
+# importing required modules
+import numpy as np
+
+## Config
+# directories and file name
+file_directory = 'C:/MUAR - 2/Reconeixement de formes i Machine Learning/NL/'
+extractedText_folder = 'ExtractedText/'
+
+languages = [
+    'Spanish',
+    'English',
+    'German',
+    'French'
+]
+
+columnsTitles = ['num_a', 
+                 'num_sch',
+                 'num_3vowels'
+                 ]
+
+def countTripleVowels(text):
+    vowels = 'aeiouAEIOU'
+    count = 0
+    for i in range(len(text) - 2):
+        if text[i] in vowels and text[i+1] in vowels and text[i+2] in vowels:
+            count += 1
+    return count
+
+for language in languages:
+    with open(file_directory + extractedText_folder + language + '.txt', 'r') as file:
+        lines = file.readlines()
+
+    all_obs_feat_list = []
+    for line in lines:
+
+        num_a = line.count('a')
+        #repetir con demás letras
+        num_sch = line.count('sch')
+        #repetir con demás conjuntos de letras
+        num_triple_vowels = countTripleVowels(line)
+        #reptir con consonantes
+        #etc
+
+        line_feat_list = [num_a,
+                          num_sch,
+                          num_triple_vowels
+                            ]
+
+
+
+
