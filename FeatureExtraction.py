5d35a0a7b3e76b06078d9e339c722fbaeada38f6
antonio.m.f <antonio.martinez.f@estudiantat.upc.edu>
2024-04-13 13:52:26 +0200

    Adding ratio vocal-consonant + creation dictionary

--- FeatureExtraction.py
+++ FeatureExtraction.py
@@ -1,6 +1,11 @@
 # importing required modules
 import numpy as np
 
+
+VOWELS = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'}
+CONSONANTS = {'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z',
+              'B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z'}
+
 ## Config
 # directories and file name
 file_directory = 'C:/MUAR - 2/Reconeixement de formes i Machine Learning/NL/'
@@ -26,6 +31,28 @@ def countTripleVowels(text):
             count += 1
     return count
 
+def count_letters(text, letters):
+    count = 0
+    for char in text:
+        if char in letters:
+            count += 1
+    return count
+
+def count_vowels(text):
+    return count_letters(text, VOWELS)
+
+def count_consonants(text):
+    return count_letters(text, CONSONANTS)
+
+def calculate_vowel_consonant_ratio(text):
+    vowel_count = count_vowels(text)
+    consonant_count = count_consonants(text)
+    if consonant_count == 0:
+        return "No consonants found. Cannot calculate ratio."
+    else:
+        return vowel_count / consonant_count
+
+
 for language in languages:
     with open(file_directory + extractedText_folder + language + '.txt', 'r') as file:
         lines = file.readlines()
@@ -34,9 +61,9 @@ for language in languages:
     for line in lines:
 
         num_a = line.count('a')
-        #repetir con demás letras
+        #repetir con demï¿½s letras
         num_sch = line.count('sch')
-        #repetir con demás conjuntos de letras
+        #repetir con demï¿½s conjuntos de letras
         num_triple_vowels = countTripleVowels(line)
         #reptir con consonantes
         #etc
