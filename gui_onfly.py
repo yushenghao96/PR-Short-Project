8d62255278a0f2f51ce170d08f332881f9580e4f
antonio.m.f <antonio.martinez.f@estudiantat.upc.edu>
2024-05-01 16:05:11 +0200

    Changues on gui

--- gui_onfly.py
+++ gui_onfly.py
@@ -12,8 +12,8 @@ import pickle
 
 #List and dict(obtain them from other file separated in the future)
 
-languages = [
-]
+languages = []
+color_map = {'German': 'red', 'English': 'green', 'Spanish': 'blue', 'French': 'yellow'}
 
 n_languages = 4
 
@@ -40,7 +40,6 @@ class GUI(tk.Tk):
         
         #Create languages list
         for i in range(n_languages):
-            print(i)
             lan = self.loaded_le.inverse_transform([i])
             languages.append(lan[0])
 
@@ -61,6 +60,9 @@ class GUI(tk.Tk):
         bottom_frame = tk.Frame(left_frame)
         bottom_frame.pack(side="bottom", fill="both", expand=True)
         
+        mid_left_frame = tk.Frame(bottom_frame)
+        mid_left_frame.pack(side="top",fill="both",expand=True)
+        
         right_frame = tk.Frame(self)
         right_frame.grid(row=0, column=1, sticky="nsew")
 
@@ -72,14 +74,22 @@ class GUI(tk.Tk):
         #self.text_box.grid(column=0, row=2, pady=10, padx=10) 
         self.text_box.focus()
         '''
+        #Text Box widget
         self.text_box = ttk.Entry(upper_frame)
         self.text_box.pack(padx=10, pady=10, fill=tk.BOTH, expand=True)
+        self.text_box.insert(0,'Enter your text here...')
+        self.text_box.config(foreground='grey')  # Set text color to grey
         self.text_box.bind("<KeyRelease>", self.calculate_model)
-        
+        #self.text_box.bind("<FocusOut>", self.on_focus_out(self.text_box))
+        self.text_box.bind("<FocusIn>",self.on_entry_click)       
         
         self.fig, self.ax = plt.subplots(figsize=(7,5)) #Adjust plot size 
         self.plot_canvas = FigureCanvasTkAgg(self.fig, master=right_frame)
         self.plot_canvas.get_tk_widget().pack(padx=10, pady=10, fill=tk.BOTH, expand=True)
+
+        #Mid widget
+        self.finalPrediction = tk.Entry(mid_left_frame,state='readonly')
+        self.finalPrediction.pack(padx=10,pady=10,fill=tk.BOTH,expand=True)
 
         #Test widgets in botton part
         self.letters_count = tk.Text(bottom_frame)
@@ -94,6 +104,12 @@ class GUI(tk.Tk):
         self.calculate_model()
         self.protocol("WM_DELETE_WINDOW", self.on_close)
     
+    def on_entry_click(self,event=None):
+        entry = self.text_box
+        if entry.get() == 'Enter your text here...':
+            entry.delete(0, tk.END)
+            entry.config(foreground='black')
+
     def update_plot(self,text,probabilistic_list, event=None):
         # Clear previous plot
         self.ax.clear()
@@ -109,23 +125,27 @@ class GUI(tk.Tk):
                 i += 1
         
         sorted_assoc = sorted(lan_dict.items(), key=lambda x: x[1], reverse=True)
-
         # Plot based on putuation of languages
         
-        #self.flag.set(True)
-        self.ax.bar([x[0] for x in sorted_assoc[:4]], [x[1] for x in sorted_assoc[:4]])
-        self.ax.set_xlabel('Values')
-        self.ax.set_ylabel('Random Numbers')
-        self.ax.set_title('Language example output')
+        bars = self.ax.bar([x[0] for x in sorted_assoc[:4]], [x[1] for x in sorted_assoc[:4]],
+                   color=[color_map[x[0]] for x in sorted_assoc[:4]])
+
+        self.ax.set_xlabel('Languages')
+        self.ax.set_ylabel('Languages probabilities')
+        self.ax.set_title('Probabilities of Languages')
+
+        self.finalPrediction.config(state='normal')
+        self.finalPrediction.delete(0,tk.END)
+        self.finalPrediction.insert(tk.END, "The input language is likely to be: ")
+        self.finalPrediction.insert(tk.END,sorted_assoc[0][0])
+        self.finalPrediction.config(state='readonly')
+
         self.letters_count.delete(1.0, tk.END)
         self.letters_count.insert(tk.END, "Random numbers generated:\n")
         for idx, rnd in enumerate(sorted_assoc):
             self.letters_count.insert(tk.END, f"{rnd[0]}: {rnd[1]:.2f}\n")
-            
-        #else:
-        #    self.flag.set(False)
-        
-        # Redraw canvas
+
+        self.ax.legend(bars, [x[0] for x in sorted_assoc[:4]])
         self.plot_canvas.draw()
     
     def calculate_model(self,event=None):
